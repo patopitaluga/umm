1336a32b05784d51ca47c6cd6cc91271459f11d6
Pato Pitaluga <patricio.pitaluga@gmail.com>
2020-04-12 22:24:36 -0300

    Added static builder for gh-pages

---
+++ staticbuild.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
+if (!fs.existsSync('./static-build')) {
+  fs.mkdirSync('./static-build');
+}
+
+/**
+ * Look ma, it's cp -R.
+ * @param {string} src The path to the thing to copy.
+ * @param {string} dest The path to the new copy.
+ */
+const copyRecursiveSync = (src, dest) => {
+  var exists = fs.existsSync(src);
+  var stats = exists && fs.statSync(src);
+  var isDirectory = exists && stats.isDirectory();
+  if (isDirectory) {
+    if (!fs.existsSync(dest))
+      fs.mkdirSync(dest);
+    fs.readdirSync(src).forEach((childItemName) => {
+      copyRecursiveSync(
+        path.join(src, childItemName),
+        path.join(dest, childItemName)
+      );
+    });
+  } else {
+    fs.copyFileSync(src, dest);
+  }
+};
+
+copyRecursiveSync('./public', './static-build');
+copyRecursiveSync('./views', './static-build');
