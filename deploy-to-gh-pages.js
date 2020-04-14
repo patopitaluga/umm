354cbdfce56ac27e8014b55ddd645584cd2b0ef5
Pato Pitaluga <patricio.pitaluga@gmail.com>
2020-04-14 20:25:19 -0300

    Update

--- deploy-to-gh-pages.js
+++ deploy-to-gh-pages.js
@@ -52,10 +52,10 @@ rimraf('static-build', async () => {
     fs.mkdirSync('static-build');
 
   await exec2('npm run build')
-    .catch((_err) => console.log(1, _err));
+    .catch((_err) => { console.log(1, _err); process.exit(0) });
 
   await exec2('git clone -b gh-pages --single-branch https://github.com/patopitaluga/umm.git static-build')
-    .catch((_err) => console.log(2, _err));
+    .catch((_err) => { console.log(2, _err); process.exit(0) });
 
   files = fs.readdirSync('public');
   files.forEach(function(file) {
@@ -68,20 +68,20 @@ rimraf('static-build', async () => {
   copyFolderRecursiveSync('public/memes', 'static-build');
 
   await exec2('cd static-build')
-    .catch((_err) => console.log(3, _err));
+    .catch((_err) => { console.log(3, _err); process.exit(0) });
   await exec2('git add .')
-    .catch((_err) => console.log(4, _err));
+    .catch((_err) => { console.log(4, _err); process.exit(0) });
   await exec2('git commit -am "Update"')
-    .catch((_err) => console.log(5, _err));
+    .catch((_err) => { console.log(5, _err); process.exit(0) });
   await exec2('git push origin gh-pages')
-    .catch((_err) => console.log(6, _err));
+    .catch((_err) => { console.log(6, _err); process.exit(0) });
   await exec2('cd ..')
-    .catch((_err) => console.log(7, _err));
+    .catch((_err) => { console.log(7, _err); process.exit(0) });
 
   rimraf('static-build', async () => {
     console.log('gh-pages updated. See https://patopitaluga.github.io/umm/ ')
 
     await exec2('open https://patopitaluga.github.io/umm/')
-      .catch((_err) => console.log(9, _err));
+      .catch((_err) => { console.log(8, _err); process.exit(0) });
   });
 });
